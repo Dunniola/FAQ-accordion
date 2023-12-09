9fceb601a693d6d33ffdc5ebac5ced3c812ddae4
Dunniola <oladunniedu611@gmail.com>
2023-12-09 23:01:50 +0100

    Added new file

---
+++ README.md
@@ -0,0 +1,56 @@
+# Frontend Mentor - Social proof section solution
+
+This is a solution to the [Social proof section challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/social-proof-section-6e0qTv_bA). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the section depending on their device's screen size
+
+### Screenshot
+Active state
+![](./design/active-states.jpg)
+Desktop design
+![](./design/desktop-design.jpg)
+desktop preview
+![](./design/desktop-preview.jpg)
+Mobile design
+![](./design/mobile-design.jpg)
+
+
+
+### Links
+
+- Solution URL: [View Demo](https://github.com/Dunniola/FAQ-accordion.git)
+- Live Site URL: [Netlify App](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- React js
+
+## Author
+
+- Website - [Dunniola](https://www.your-site.com)
+- Frontend Mentor - [@Dunniola](https://www.frontendmentor.io/profile/Dunniola)
+- Twitter - [@Eduoladunni](https://www.twitter.com/Eduoladunni)
---
+++ src/App.tsx
@@ -0,0 +1,109 @@
+import { useState, useRef, useEffect } from "react";
+import "./App.css";
+import "../src/assets/images/icon-minus.svg";
+import iconStar from "../src/assets/images/icon-star.svg"
+import  "../src/assets/images/icon-plus.svg";
+import  "../src/assets/images/background-pattern-desktop.svg";
+import  "../src/assets/images/background-pattern-mobile.svg";
+
+const FAQItem = ({question, answer, toggle, onToggle}:any) => {
+  const[heightEl, setHeightEl] = useState<string | undefined>(undefined);
+  const refHeight = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (refHeight.current) {
+      setHeightEl(`${refHeight.current.scrollHeight}px`);
+    }else{
+      setHeightEl(undefined);
+    }
+  }, []);
+ 
+  
+  return (
+    
+      <div>
+        <div className="flex">
+          <h4 className="question">{question}</h4>
+          <button onClick={()=> onToggle(question)} className="button">
+            <img
+              src={
+                toggle===question
+                  ? '../src/assets/images/icon-minus.svg'
+                  : '../src/assets/images/icon-plus.svg'
+              }
+            />
+          </button>
+        </div>
+        <div
+          className={toggle === question ? 'answer' : ''}
+          style={{ height: toggle === question ? `${heightEl}` : '0px' }}
+          ref={refHeight}
+        >
+          <p className={toggle === question  ? 'non-active' : 'active'}>{answer}</p>
+        </div>
+      </div>
+    );
+  };
+
+
+
+const App = () => {
+  const[toggle, setToggle] = useState(null);
+
+  const handleToggle = (question: null) =>{
+    setToggle((prevToggle)=> (prevToggle === question ? null : question));
+  }
+  return (
+    <div>
+      <div className="main-container">
+        <div className="container ">
+          <img src={iconStar} alt=""></img>
+          <h1> FAQs</h1>
+        </div>
+        
+        <div className="one">
+          <FAQItem
+            question="What is Frontend Mentor, and how will it help me?"
+            answer="Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building."
+            toggle={toggle}
+            onToggle={handleToggle}
+          />
+        </div>
+
+       <hr />
+
+        <div className="two">
+          <FAQItem
+            question="Is Frontend Mentor free?"
+            answer="Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels."
+            toggle={toggle}
+            onToggle={handleToggle}
+          />
+             
+        </div>
+        <hr />
+
+        <div className="three">
+          <FAQItem
+            question="Can I use Frontend Mentor projects in my portfolio?"
+            answer="Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!"
+            toggle={toggle}
+            onToggle={handleToggle}
+          />
+        </div>
+        <hr />
+
+        <div className="four">
+          <FAQItem
+            question="How can I get help if I'm stuck on a Frontend Mentor challenge?"
+            answer="The best place to get help is inside Frontend Mentor's Discord community. There's a help where you can ask questions and seek support from other community members."
+            toggle={toggle}
+            onToggle={handleToggle}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App
