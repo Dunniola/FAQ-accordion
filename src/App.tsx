77aab4f01608750c152d30520ef6f0aadbdda80c
Dunniola <oladunniedu611@gmail.com>
2023-12-10 13:11:28 +0100

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { useState, useRef, useEffect } from "react";
 
 import "./App.css";
-import  "../assets/images/background-pattern-desktop.svg";
-import "../assets/images/background-pattern-mobile.svg";
-import "../assets/images/icon-minus.svg";
-import  "../assets/images/icon-plus.svg";
-import iconStar from "../assets/images/icon-star.svg";
+import  "../public/images/background-pattern-desktop.svg";
+import "../public/images/background-pattern-mobile.svg";
+import "../public/images/icon-minus.svg";
+import  "../public/images/icon-plus.svg";
+import iconStar from "../public/images/icon-star.svg";
 const FAQItem = ({question, answer, toggle, onToggle}:any) => {
   const[heightEl, setHeightEl] = useState<string | undefined>(undefined);
   const refHeight = useRef<HTMLDivElement>(null);
@@ -26,7 +26,7 @@ const FAQItem = ({question, answer, toggle, onToggle}:any) => {
           <h4 className="question">{question}</h4>
           <button onClick={()=> onToggle(question)} className="button">
               
-            <img src={toggle===question ? "../assets/images/icon-minus.svg": "../assets/images/icon-plus.svg"} alt=""></img>
+            <img src={toggle===question ? "../public/images/icon-minus.svg": "../public/images/icon-plus.svg"} alt=""></img>
       
           </button>
         </div>
